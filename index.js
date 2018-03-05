a98a282b38e0761bc10a2ea4c812bb275e614e1f
Louis Shi <louis.shi@uconn.edu>
2018-03-05 14:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 function theBeatlesPlay (musicians, instruments) {
-  var array = []
+  var array = [];
   for ( var i = 0; i < musicians.length; i++) {
-   array.push(`${musicians[i]} plays ${instruments[i]}`)
+   array.push(`${musicians[i]} plays ${instruments[i]}`);
   }
-  return array
+  return array;
 }
 function johnLennonFacts (array) {
-  var i = 0
+  var i = 0;
   while (array[i]) {
-   array[i] = array[i].concat("!!!")
-    i++
+   array[i] = array[i].concat("!!!");
+    i++;
   }
-  return array
+  return array;
 }
+
 function iLoveTheBeatles(number) {
-  var array = []
+  var array = [];
   do {
-      array.push("I love the Beatles!")
-      console.log(array)
-      number++
+      array.push("I love the Beatles!");
+      console.log(array);
+      number++;
   } while (number < 15)
-return array
+return array;
 }
